576859ace1308682453f90dfe0353b4b9338c8d0
Felipe Marchant F. Soares <felipemarchant.dev@gmail.com>
2025-01-21 19:43:53 -0300

    feat :tada: started project!

---
+++ src/main/java/br/com/felipemarchant/categora/CategoraApplication.java
@@ -0,0 +1,13 @@
+package br.com.felipemarchant.categora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CategoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CategoraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/felipemarchant/categora/CategoraApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.felipemarchant.categora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CategoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
