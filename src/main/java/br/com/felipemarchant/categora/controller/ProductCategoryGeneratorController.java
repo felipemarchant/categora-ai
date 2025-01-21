09117402f1163ef132cca5d94999e05c11040df6
Felipe Marchant F. Soares <felipemarchant.dev@gmail.com>
2025-01-21 20:36:10 -0300

    feat :sparkles: add product categorizer endpoint

--- src/main/java/br/com/felipemarchant/categora/controller/ProductCategoryGeneratorController.java
+++ src/main/java/br/com/felipemarchant/categora/controller/ProductCategoryGeneratorController.java
@@ -1,5 +1,8 @@
 package br.com.felipemarchant.categora.controller;
 
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.prompt.ChatOptionsBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("category")
 public class ProductCategoryGeneratorController {
 
+    private final ChatClient chatClient;
+
+    public ProductCategoryGeneratorController(final ChatClient.Builder chatClientBuilder) {
+        chatClient = chatClientBuilder.build();
+    }
+
     @GetMapping
-    public String category() {
+    public String category(final String product) {
         var system = """
         You are a product categorizer and should only respond with the product category.
         Category has unique id, name, description, created at, updated at
@@ -29,6 +38,16 @@ public class ProductCategoryGeneratorController {
         Question: Soccer ball
         Answer: [{"id":1,"name":"Sports","description":"Sport description","createdAt":"2025-01-21T10:10:00Z","updatedAt":"2025-01-21T10:10:00Z"}]
         """;
-        return "";
+
+        return chatClient.prompt()
+                .advisors(new SimpleLoggerAdvisor())
+                .system(system)
+                .user(product)
+                .options(ChatOptionsBuilder
+                        .builder()
+                        .withTemperature(0.8)
+                        .build())
+                .call()
+                .content();
     }
 }
