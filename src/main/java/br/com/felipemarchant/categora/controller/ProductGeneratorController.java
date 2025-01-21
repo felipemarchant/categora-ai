10ee976837fcb543b10c1c42f737eb9383bb7dcd
Felipe Marchant F. Soares <felipemarchant.dev@gmail.com>
2025-01-21 20:03:23 -0300

    feat :sparkles: add product generation endpoint

--- src/main/java/br/com/felipemarchant/categora/controller/ProductGeneratorController.java
+++ src/main/java/br/com/felipemarchant/categora/controller/ProductGeneratorController.java
@@ -1,5 +1,6 @@
-package controller;
+package br.com.felipemarchant.categora.controller;
 
+import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("product")
 public class ProductGeneratorController {
 
+    private final ChatClient chatClient;
+
+    public ProductGeneratorController(final ChatClient.Builder chatClientBuilder) {
+        chatClient = chatClientBuilder.build();
+    }
+
     @GetMapping
     public String generateProduct() {
-        return "";
+        return chatClient.prompt()
+                .user("Generate 5 products")
+                .call()
+                .content();
     }
 }
